18a8faf08fd59cabc087a186325c10a83f2aadb2
VINO <VINO@DESKTOP-M39TTO3>
2018-10-13 13:01:02 +0530

    2nd push

--- MagentoTest/src/test/java/MagentoTest.java
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest {
@@ -11,7 +12,7 @@ public class MagentoTest {
 public void posCredentials() throws Exception 
 {
 		String url="http://www.magento.com";
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(url);
